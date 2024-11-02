77bd5d4d7edfb5696c30ab56654240bd309b21ce
iamwinner422 <iamwinner422@gmail.com>
2024-11-02 14:06:54 +0000

    construction

--- app/src/main/java/com/example/learnjc/MainActivity.kt
+++ app/src/main/java/com/example/learnjc/MainActivity.kt
@@ -6,15 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,11 +62,26 @@ fun Greeting(name: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    //Où R est une classe générée automatiquement, drawable, le sous-dossier
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier){
+        Image(painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        Greeting(name = message, from = from, modifier = Modifier.fillMaxSize()
+            .padding(8.dp)
+        )
+    }
 
-@Preview(showBackground = true, showSystemUi = true)
+}
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LearnJCTheme {
-        Greeting("erwinn", from = "Coco Chanel")
+        GreetingImage("Hello My", from = "Coco Chanel")
     }
 }
