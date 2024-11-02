8fc3beb2ffde40ada18084ffac9752b2a9ef3b29
iamwinner422 <iamwinner422@gmail.com>
2024-11-02 13:35:23 +0000

    First version of Birthday card

--- app/src/main/java/com/example/learnjc/MainActivity.kt
+++ app/src/main/java/com/example/learnjc/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learnjc.ui.theme.LearnJCTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +29,8 @@ class MainActivity : ComponentActivity() {
             LearnJCTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android My Nigaa",
+                        name = "Android My Nigga",
+                        from = "Coco",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +40,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Bonjour $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp),
+    ) {
+        Text(
+            text = "Joyeux Anniversaire $name!",
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier.padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     LearnJCTheme {
-        Greeting("erwinn")
+        Greeting("erwinn", from = "Coco Chanel")
     }
 }
